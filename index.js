bf8d1b9460982e164a3bef6262fb0b957b116730
stefanozuffi <stefano.zuffi06@outlook.com>
2025-09-07 14:44:59 +0200

    initialization and preparation of the project

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const blogServer = express()
+const port = 3000
+
+// Listen
+blogServer.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`)
+})
+
+// Elementi Statici
+blogServer.use(express.static('public'))
+
+
+// Creiamo l'array dei cinque post
+const postsArray = [
+    {
+        id: 1,
+        title: 'Post 1',
+        content: 'Contenuto del post 1',
+        img: 'images/ciambellone.jpeg',
+        tags: ['cucina', 'dolci', 'colazione']
+    },
+    {
+        id: 2,
+        title: 'Post 2',
+        content: 'Contenuto del post 2',
+        img: 'images/cracker_barbabietola.jpeg',
+        tags: ['cucina', 'salato', 'snack']
+    },
+    {
+        id: 3,
+        title: 'Post 3',
+        content: 'Contenuto del post 3',
+        img: 'images/pane_fritto_dolce.jpeg',
+        tags: ['cucina', 'dolci', 'snack']
+    },
+    {
+        id: 4,
+        title: 'Post 4',
+        content: 'Contenuto del post 4',
+        img: 'images/pasta_barbabietola.jpeg',
+        tags: ['cucina', 'salato', 'primi']
+    },
+    {
+        id: 5,
+        title: 'Post 5',
+        content: 'Contenuto del post 5',
+        img: 'images/torta_paesana.jpeg',
+        tags: ['cucina', 'dolci', 'dessert']
+    }
+   
+]
+
+
+// ROUTES FOR POSTS
